e6e72c91fab7447b97baebf7dbec7cb196391bd0
sanzida-mim <mim.3ingenious@gmail.com>
2025-04-04 00:02:05 +0600

    Displayed winner msg upon game completion.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll('.box');
 let resetBtn = document.querySelectorAll('#reset-btn');
+let newGameBtn = document.querySelector('#new-btn');
+let msgContainer = document.querySelector('.msg-container');
+let msg = document.querySelector('#msg');
 
 let turnO = true;
 
@@ -31,6 +34,11 @@ boxes.forEach((box) => {
     })
 })
 
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove('hide');
+};
+
 const checkWinner = () => {
     for(let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
@@ -40,6 +48,7 @@ const checkWinner = () => {
         if (pos1Val != '' && pos2Val != '' && pos3Val != '') {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 console.log('winner', pos1Val);
+                showWinner(pos1Val);
             }
         }
     }
